d3c753d09eeece29413f455ed390fa58f54e1b0a
sim <sim@terra.money>
2021-12-04 04:40:28 +0900

    test: Try to truncate short string

--- test/text.test.ts
+++ test/text.test.ts
@@ -5,4 +5,5 @@ const ADDRESS = "terra1x46rqay4d3cssq8gxxvqz8xt6nwlz4td20k38v"
 test("truncate", () => {
   expect(truncate(ADDRESS)).toBe("terra1...20k38v")
   expect(truncate(ADDRESS, [6, 3])).toBe("terra1...38v")
+  expect(truncate("short")).toBe("short")
 })
