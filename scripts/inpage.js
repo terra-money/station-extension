571011cc56621266481e755a1f9853e1c750796c
sim <sim@terra.money>
2022-01-17 16:31:58 +0900

    build: Update extension name

--- scripts/inpage.js
+++ scripts/inpage.js
@@ -4,13 +4,13 @@ window.isTerraExtensionAvailable = true
 // for multiple extension support
 // ---------------------------------------------
 const TERRA_STATION_INFO = {
-  name: 'Terra Station',
-  identifier: 'station',
-  icon: 'https://assets.terra.money/icon/station-extension/icon.png',
+  name: "Terra Station Wallet",
+  identifier: "station",
+  icon: "https://assets.terra.money/icon/station-extension/icon.png",
 }
 
 if (
-  typeof window.terraWallets !== 'undefined' &&
+  typeof window.terraWallets !== "undefined" &&
   Array.isArray(window.terraWallets)
 ) {
   window.terraWallets.push(TERRA_STATION_INFO)
