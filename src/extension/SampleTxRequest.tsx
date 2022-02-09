c861a48b81686fd03ddfaf98c0ee381dd2fa7b3a
sim <sim@terra.money>
2022-02-09 18:31:54 +0900

    Handle long memo from a tx request

---
+++ src/extension/SampleTxRequest.tsx
@@ -0,0 +1,16 @@
+import { MsgSend } from "@terra-money/terra.js"
+
+const id = Date.now()
+const address = "terra1x46rqay4d3cssq8gxxvqz8xt6nwlz4td20k38v"
+const memo =
+  "Loremipsumdolorsitamet,consecteturadipisicingelit,seddoeiusmodtemporincididuntutlaboreetdoloremagnaaliqua.Utenimadminimveniam,quisnostrudexercitationullamcolaborisnisiutaliquipexeacommodoconsequat.Duisauteiruredolorinreprehenderitinvoluptatevelitessecillumdoloreeufugiatnullapariatur.Excepteursintoccaecatcupidatatnonproident,suntinculpaquiofficiadeseruntmollitanimidestlaborum."
+
+const SampleTxRequest = {
+  id,
+  timestamp: new Date(id),
+  origin: "https://station.terra.money",
+  requestType: "post" as const,
+  tx: { msgs: [new MsgSend(address, address, "1uusd")], memo },
+}
+
+export default SampleTxRequest
