a1e23c44fa33ee9bfef7f36c9102f71d9911492e
sim <sim@terra.money>
2022-10-28 21:21:11 +0900

    Bump extension version

--- src/extension/auth/SwitchWalletButton.tsx
+++ src/extension/auth/SwitchWalletButton.tsx
@@ -3,9 +3,9 @@ import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet"
 import { useAddress } from "data/wallet"
 import { ModalButton } from "components/feedback"
 import { useAuth } from "auth"
+import { useNetworkState } from "../../data/wallet"
 import SwitchWallet from "./SwitchWallet"
 import styles from "./ConnectedWallet.module.scss"
-import { useNetworkState } from "../../data/wallet"
 
 const SwitchWalletButton = () => {
   const { t } = useTranslation()
@@ -13,8 +13,10 @@ const SwitchWalletButton = () => {
   const { wallets } = useAuth()
   const [network] = useNetworkState()
 
-  return (wallets.length < 2 && network !== "localterra") ||
-    wallets.length === 0 ? null : (
+  const switchable = wallets.length > 1 || network === "localterra"
+  if (!switchable) return null
+
+  return (
     <ModalButton
       title={t("Switch wallet")}
       renderButton={(open) => (
