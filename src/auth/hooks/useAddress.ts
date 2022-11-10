0bc876cd3eee4c2d13483523ed0a528b5ab0ea09
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2022-11-10 11:51:28 +0100

    Show assets on multiple chains
    Still beta and the code needs an huge cleanup

--- src/auth/hooks/useAddress.ts
+++ src/auth/hooks/useAddress.ts
@@ -1,8 +1,24 @@
+import { useConnectedWallet } from "@terra-money/use-wallet"
+import { bech32 } from "bech32"
 import useAuth from "./useAuth"
 
 const useAddress = () => {
   const { wallet } = useAuth()
   return wallet?.address
+}
+
+// TODO: fetch from assets.terra.money
+const prefixes = ["terra", "osmo"]
+
+export const useInterchainAddresses = () => {
+  const connected = useConnectedWallet()
+  const { wallet } = useAuth()
+  const address = wallet?.address ?? connected?.terraAddress
+  if (!address) return []
+  const { words } = bech32.decode(address)
+  const addresses = prefixes.map((prefix) => bech32.encode(prefix, words))
+  console.log(addresses)
+  return addresses
 }
 
 export default useAddress
