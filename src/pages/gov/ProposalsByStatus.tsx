9e6067f4c3bfeea02ea21c7579b91db5e120a528
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2022-11-25 15:17:54 +0100

    Add interchain station governance (#128)
    * Add interchain governance
    
    * cleanup, change back button
    
    * remove contract page
    
    * Fix governance
    
    * fix ts-errors

--- src/pages/gov/ProposalsByStatus.tsx
+++ src/pages/gov/ProposalsByStatus.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react"
 import { useTranslation } from "react-i18next"
-import { reverse } from "ramda"
 import { Proposal } from "@terra-money/terra.js"
 import { combineState, useIsClassic } from "data/query"
 import { useProposals, useProposalStatusItem } from "data/queries/gov"
@@ -11,17 +10,20 @@ import { Toggle } from "components/form"
 import ProposalItem from "./ProposalItem"
 import GovernanceParams from "./GovernanceParams"
 import styles from "./ProposalsByStatus.module.scss"
+import { useNetworkName } from "data/wallet"
+import ChainFilter from "components/layout/ChainFilter"
 
 const ProposalsByStatus = ({ status }: { status: Proposal.Status }) => {
   const { t } = useTranslation()
   const isClassic = useIsClassic()
 
-  const [showAll, setShowAll] = useState(!isClassic)
-  const toggle = () => setShowAll((state) => !state)
+  const { data: whitelistData, ...whitelistState } = useTerraAssets<{
+    [key: string]: number[]
+  }>("/station/proposals.json")
+  const whitelist = whitelistData?.[networkName]
 
-  const { data: whitelist, ...whitelistState } = useTerraAssets<number[]>(
-    "/station/proposals.json"
-  )
+  const [showAll, setShowAll] = useState(!!whitelist)
+  const toggle = () => setShowAll((state) => !state)
 
   const { data, ...proposalState } = useProposals(status)
   const { label } = useProposalStatusItem(status)
@@ -33,35 +35,61 @@ const ProposalsByStatus = ({ status }: { status: Proposal.Status }) => {
 
     const proposals =
       status === Proposal.Status.PROPOSAL_STATUS_VOTING_PERIOD && !showAll
-        ? data.filter(({ id }) => whitelist.includes(id))
+        ? data.filter(
+            ({ prop, chain }) =>
+              chain !== "phoenix-1" || whitelist?.includes(prop.id)
+          )
         : data
 
-    return !proposals.length ? (
-      <>
-        <Card>
-          <Empty>
-            {t("No proposals in {{label}} period", {
-              label: label.toLowerCase(),
-            })}
-          </Empty>
-        </Card>
-        <GovernanceParams />
-      </>
-    ) : (
-      <>
-        <section className={styles.list}>
-          {reverse(proposals).map((item) => (
-            <Card
-              to={`/proposal/${item.id}`}
-              className={styles.link}
-              key={item.id}
-            >
-              <ProposalItem proposal={item} showVotes={!showAll} />
-            </Card>
-          ))}
-        </section>
+    proposals.sort(
+      (a, b) =>
+        (b.prop.voting_start_time || b.prop.submit_time).getTime() -
+        (a.prop.voting_start_time || a.prop.submit_time).getTime()
+    )
 
-        <GovernanceParams />
+    return (
+      <>
+        <ChainFilter all>
+          {(chain) => {
+            const filtered = proposals.filter(
+              (p) => !chain || p.chain === chain
+            )
+            return !filtered.length ? (
+              <>
+                <Card>
+                  <Empty>
+                    {t("No proposals in {{label}} period", {
+                      label: label.toLowerCase(),
+                    })}
+                  </Empty>
+                </Card>
+                {chain && <GovernanceParams chain={chain} />}
+              </>
+            ) : (
+              <>
+                <section className={styles.list}>
+                  {filtered.map(({ prop, chain }) => (
+                    <Card
+                      to={`/proposal/${chain}/${prop.id}`}
+                      className={styles.link}
+                      key={prop.id}
+                    >
+                      <ProposalItem
+                        proposal={prop}
+                        chain={chain}
+                        showVotes={
+                          status ===
+                          Proposal.Status.PROPOSAL_STATUS_VOTING_PERIOD
+                        }
+                      />
+                    </Card>
+                  ))}
+                </section>
+                {chain && <GovernanceParams chain={chain} />}
+              </>
+            )
+          }}
+        </ChainFilter>
       </>
     )
   }
@@ -69,13 +97,14 @@ const ProposalsByStatus = ({ status }: { status: Proposal.Status }) => {
   return (
     <Fetching {...state}>
       <Col>
-        {isClassic && status === Proposal.Status.PROPOSAL_STATUS_VOTING_PERIOD && (
-          <section>
-            <Toggle checked={showAll} onChange={toggle}>
-              {t("Show all")}
-            </Toggle>
-          </section>
-        )}
+        {!!whitelist &&
+          status === Proposal.Status.PROPOSAL_STATUS_VOTING_PERIOD && (
+            <section>
+              <Toggle checked={showAll} onChange={toggle}>
+                {t("Show all")}
+              </Toggle>
+            </section>
+          )}
 
         {render()}
       </Col>
