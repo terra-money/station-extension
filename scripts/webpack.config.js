eb0277f74d47bd371b2d85cbc1c31cd3216b9e88
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-01-03 21:15:07 +0100

    Update to manifest v3

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -1,16 +1,22 @@
-const path = require("path");
+const path = require("path")
+const webpack = require("webpack")
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
+  mode: process.env.NODE_ENV || "development",
   devtool: "inline-source-map",
   bail: true,
   entry: {
     contentScript: path.join(__dirname, "contentScript.js"),
     background: path.join(__dirname, "background.js"),
-    inpage: path.join(__dirname, "inpage.js")
+    inpage: path.join(__dirname, "inpage.js"),
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      global: {},
+    }),
+  ],
   output: {
     path: path.join(__dirname, "..", "build"),
-    filename: "[name].js"
-  }
-};
+    filename: "[name].js",
+  },
+}
