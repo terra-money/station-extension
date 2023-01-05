7d26ed2dac1548931fa88f2594ace7eb995790ca
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-01-05 15:30:10 +0100

    Fix initial loading

--- src/app/InitChains.tsx
+++ src/app/InitChains.tsx
@@ -2,6 +2,7 @@ import axios from "axios"
 import { ASSETS } from "config/constants"
 import { ChainsProvider } from "data/queries/chains"
 import { PropsWithChildren, useEffect, useState } from "react"
+import NetworkLoading from "./NetworkLoading"
 
 const InitChains = ({ children }: PropsWithChildren<{}>) => {
   const [data, setData] = useState<any>()
@@ -15,7 +16,8 @@ const InitChains = ({ children }: PropsWithChildren<{}>) => {
     })()
   }, [])
 
-  if (!data) return null
+  if (!data)
+    return <NetworkLoading title="Connecting to available networks..." />
 
   return <ChainsProvider value={data}>{children}</ChainsProvider>
 }
