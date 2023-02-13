2c28e7bbd56a32d0f10708effe13eb269085a9b2
plubber <51789398+ericHgorski@users.noreply.github.com>
2023-02-13 11:55:55 -0500

    Feat/loading optimizations (#267)
    * fix: receive page bug on desktop
    
    * wip: network optimization
    
    * wip: network optimization 2
    
    * wip: optimize InitChains comp
    
    * wip: optimize InitNetworks  comp 2
    
    * wip: optimize InitNetworks  comp 3
    
    * wip: optimize InitNetworks  comp 4
    
    * cleanup
    
    * fix: loading bug
    
    * fix: loading bug 2
    
    * cleanup
    
    * Fix network validation requests
    
    * Add page loader during validation
    
    ---------
    
    Co-authored-by: Alessandro Candeago <54709706+alecande11@users.noreply.github.com>

--- src/app/InitWallet.tsx
+++ src/app/InitWallet.tsx
@@ -1,19 +1,14 @@
-import { PropsWithChildren, useEffect, useMemo } from "react"
-import { QueryClient, QueryClientProvider } from "react-query"
-import { useNetworkName } from "data/wallet"
+import { PropsWithChildren, useEffect } from "react"
 import { isWallet, useAuth } from "auth"
 import Online from "./containers/Online"
 
 const InitWallet = ({ children }: PropsWithChildren<{}>) => {
   useOnNetworkChange()
-  const queryClient = useQueryClient()
-  const networkName = useNetworkName()
-
   return (
-    <QueryClientProvider client={queryClient} key={networkName}>
+    <>
       {children}
       <Online />
-    </QueryClientProvider>
+    </>
   )
 }
 
@@ -28,13 +23,4 @@ const useOnNetworkChange = () => {
   useEffect(() => {
     if (shouldDisconnect) disconnect()
   }, [disconnect, shouldDisconnect])
-}
-
-const useQueryClient = () => {
-  const name = useNetworkName()
-
-  return useMemo(() => {
-    if (!name) throw new Error()
-    return new QueryClient()
-  }, [name])
 }
