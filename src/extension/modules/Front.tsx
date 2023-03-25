29cf2a045f45705decd621b626612cc1dd93f476
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-03-25 22:20:17 +0100

    Cleanup test env

--- src/extension/modules/Front.tsx
+++ src/extension/modules/Front.tsx
@@ -35,48 +35,13 @@ const Front = () => {
     return <ConfirmPubkey origin={pubkey} />
   }
 
-  if (true) {
-    return (
-      <ConfirmNewChain
-        {...{
-          network: "mainnet",
-          origin: "https://e-money.com/",
-          id: 0,
-          chain: {
-            chainID: "emoney-3",
-            lcd: "https://emoney.validator.network/api/",
-            gasAdjustment: 1.75,
-            gasPrices: { ungm: 0.1 },
-            prefix: "emoney",
-            coinType: "118",
-            baseAsset: "ungm",
-            name: "eMoney",
-            icon: "https://raw.githubusercontent.com/cosmos/chain-registry/master/emoney/images/ngm.png",
-            // doesn't require IBC channels since it's already on all the other chains
-            explorer: {
-              address: "https://www.mintscan.io/emoney/account/{}",
-              tx: "https://www.mintscan.io/emoney/txs/{}",
-              validator: "https://www.mintscan.io/emoney/validators/{}",
-              block: "https://www.mintscan.io/emoney/blocks/id/{}",
-            },
-            tokens: [
-              {
-                token: "ungm",
-                symbol: "NGM",
-                name: "NGM",
-                icon: "https://raw.githubusercontent.com/cosmos/chain-registry/master/emoney/images/ngm.png",
-                decimals: 6,
-              },
-            ],
-          },
-        }}
-      />
-    )
+  if (chain) {
+    return <ConfirmNewChain {...chain} />
   }
-  /*
+
   if (tx) {
     return <ConfirmTx {...tx} />
-  }*/
+  }
 
   return <Wallet />
 }
