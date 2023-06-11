aa1f2d45cc4cd29bb9b47a8303f756197257f5b4
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-06-11 11:29:37 +0200

    fix chain list overlapping

--- src/pages/wallet/AssetPage.tsx
+++ src/pages/wallet/AssetPage.tsx
@@ -82,9 +82,9 @@ const AssetPage = () => {
           {symbol}
         </p>
       </section>
-      <section className={styles.chainlist}>
+      <section className={styles.chainlist__container}>
         {filteredBalances.length > 0 && (
-          <>
+          <div className={styles.chainlist}>
             <div className={styles.chainlist__title}>
               <h3>{t("Chains")}</h3>
             </div>
@@ -104,10 +104,10 @@ const AssetPage = () => {
                   </div>
                 ))}
             </div>
-          </>
+          </div>
         )}
         {filteredUnsupportedBalances.length > 0 && (
-          <>
+          <div className={styles.chainlist}>
             <div className={styles.chainlist__title}>
               <h3>{t("Unsupported Chains")}</h3>
             </div>
@@ -128,7 +128,7 @@ const AssetPage = () => {
                   </div>
                 ))}
             </div>
-          </>
+          </div>
         )}
       </section>
 
