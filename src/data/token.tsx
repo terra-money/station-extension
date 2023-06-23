3361f8d7b4680e56c38fa7850a4d04a38d13ca29
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2023-06-23 07:02:27 +0200

    style: readability

--- src/data/token.tsx
+++ src/data/token.tsx
@@ -158,8 +158,11 @@ export const useNativeDenoms = () => {
     let ibcToken = ibcDenoms[networkName]?.[denom]?.token
     const chainOrigin = ibcDenoms[networkName]?.[denom]?.chainID
     const ibcLunc =
-      chainOrigin && ibcToken === "phoenix-1:uluna" && networkName === "mainnet"
-    if (ibcLunc && ibcDenoms["classic"]?.[denom]) {
+      chainOrigin &&
+      ibcToken === "phoenix-1:uluna" &&
+      networkName === "mainnet" &&
+      ibcDenoms["classic"]?.[denom]
+    if (ibcLunc) {
       ibcToken = ibcDenoms["classic"]?.[denom]?.token
       return {
         ...whitelist["classic"][ibcToken],
