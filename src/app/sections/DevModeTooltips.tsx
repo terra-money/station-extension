d6921cfa4bd7a054d9797d662c0af64c73b21d21
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-07-06 17:04:14 +0200

    remove reference to contract page

--- src/app/sections/DevModeTooltips.tsx
+++ src/app/sections/DevModeTooltips.tsx
@@ -7,7 +7,6 @@ export const DevModeTooltip = () => {
     <article>
       <h1>{t("Developer mode enables the following")}:</h1>
       <ul>
-        <li>{t("Contracts page")}</li>
         <li>{t("Copy token addresses from your wallet")}</li>
       </ul>
     </article>
