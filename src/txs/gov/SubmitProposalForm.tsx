29629b5e259e29cc04ef2456bf09f7045c7738fb
plubber <er.h.gorski@gmail.com>
2023-07-12 17:48:26 -0400

    fix: unintended form validation

--- src/txs/gov/SubmitProposalForm.tsx
+++ src/txs/gov/SubmitProposalForm.tsx
@@ -81,7 +81,7 @@ const SubmitProposalForm = ({ chain }: { chain: string }) => {
   const { t } = useTranslation()
   const addresses = useInterchainAddresses()
   const networks = useNetwork()
-  const readNetiveDenom = useNativeDenoms()
+  const readNativeDenom = useNativeDenoms()
 
   const bankBalance = useBankBalance()
   const balance =
@@ -247,7 +247,7 @@ const SubmitProposalForm = ({ chain }: { chain: string }) => {
                 <Select {...register("spend.denom")} before>
                   {[networks[chain].baseAsset].map((denom) => (
                     <option value={denom} key={denom}>
-                      {readNetiveDenom(denom).symbol}
+                      {readNativeDenom(denom).symbol}
                     </option>
                   ))}
                 </Select>
@@ -380,7 +380,7 @@ const SubmitProposalForm = ({ chain }: { chain: string }) => {
                         .filter(({ denom }) => isDenomTerraNative(denom))
                         .map(({ denom }) => (
                           <option value={denom} key={denom}>
-                            {readNetiveDenom(denom).symbol}
+                            {readNativeDenom(denom).symbol}
                           </option>
                         ))}
                     </Select>
@@ -448,7 +448,7 @@ const SubmitProposalForm = ({ chain }: { chain: string }) => {
                 })}
                 placeholder={t(
                   `We're proposing to spend 100,000 ${
-                    readNetiveDenom(networks[chain].baseAsset).symbol
+                    readNativeDenom(networks[chain].baseAsset).symbol
                   } from the Community Pool to fund the creation of public goods for the ${
                     networks[chain].name
                   } ecosystem`
@@ -460,12 +460,12 @@ const SubmitProposalForm = ({ chain }: { chain: string }) => {
               label={
                 <TooltipIcon
                   content={`To help push the proposal to the voting period, consider depositing more ${
-                    readNetiveDenom(networks[chain].baseAsset).symbol
+                    readNativeDenom(networks[chain].baseAsset).symbol
                   } to reach the minimum ${
                     Number(minDeposit) /
-                    10 ** readNetiveDenom(networks[chain].baseAsset).decimals
+                    10 ** readNativeDenom(networks[chain].baseAsset).decimals
                   } ${
-                    readNetiveDenom(networks[chain].baseAsset).symbol
+                    readNativeDenom(networks[chain].baseAsset).symbol
                   } (optional).`}
                 >
                   {t("Initial deposit")} ({t("optional")})
