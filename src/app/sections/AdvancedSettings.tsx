cb1a92a8afe611b3ca44881414d0a81f93b16e2e
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-07-17 19:20:30 +0200

    replace keplr window object

--- src/app/sections/AdvancedSettings.tsx
+++ src/app/sections/AdvancedSettings.tsx
@@ -1,21 +1,39 @@
-import { useDevMode } from "utils/localStorage"
+import { useDevMode, useReplaceKeplr } from "utils/localStorage"
 import SettingsSelectorToggle from "components/layout/SettingsSelectorToggle"
 import { FlexColumn, GasAdjustment } from "components/layout"
 import { TooltipIcon } from "components/display"
-import { DevModeTooltip } from "./DevModeTooltips"
+import { DevModeTooltip, ReplaceKeplrTooltip } from "./DevModeTooltips"
+import { FormWarning } from "components/form"
 
 const AdvancedSettings = () => {
   const { devMode, changeDevMode } = useDevMode()
-  const options = [
-    { value: "devMode", selected: devMode, label: "Developer Mode" },
-  ]
+  const { replaceKeplr, toggleReplaceKeplr } = useReplaceKeplr()
 
   return (
     <FlexColumn gap={10}>
       <SettingsSelectorToggle
+        onChange={toggleReplaceKeplr}
+        extra={<TooltipIcon content={<ReplaceKeplrTooltip />} />}
+        options={[
+          {
+            value: "replaceKeplr",
+            selected: replaceKeplr,
+            label: "Replace Keplr",
+          },
+        ]}
+      />
+      {replaceKeplr && (
+        <FormWarning>
+          To prevent collisions between Keplr and Station, when this fature is
+          enabled uninstall or disable the Keplr extension on your browser.
+        </FormWarning>
+      )}
+      <SettingsSelectorToggle
         onChange={changeDevMode}
         extra={<TooltipIcon content={<DevModeTooltip />} />}
-        options={options}
+        options={[
+          { value: "devMode", selected: devMode, label: "Developer Mode" },
+        ]}
       />
       <GasAdjustment />
     </FlexColumn>
