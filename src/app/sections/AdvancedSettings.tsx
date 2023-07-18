aefa716e58a147c0a39e3173904aaade6823952b
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-07-18 15:46:05 +0200

    hide keplr setting (#246)

--- src/app/sections/AdvancedSettings.tsx
+++ src/app/sections/AdvancedSettings.tsx
@@ -2,15 +2,19 @@ import { useDevMode, useReplaceKeplr } from "utils/localStorage"
 import SettingsSelectorToggle from "components/layout/SettingsSelectorToggle"
 import { FlexColumn, GasAdjustment } from "components/layout"
 import { TooltipIcon } from "components/display"
-import { DevModeTooltip, ReplaceKeplrTooltip } from "./DevModeTooltips"
-import { FormWarning } from "components/form"
+import {
+  DevModeTooltip,
+  //ReplaceKeplrTooltip
+} from "./DevModeTooltips"
+//import { FormWarning } from "components/form"
 
 const AdvancedSettings = () => {
   const { devMode, changeDevMode } = useDevMode()
-  const { replaceKeplr, toggleReplaceKeplr } = useReplaceKeplr()
+  //const { replaceKeplr, toggleReplaceKeplr } = useReplaceKeplr()
 
   return (
     <FlexColumn gap={10}>
+      {/*
       <SettingsSelectorToggle
         onChange={toggleReplaceKeplr}
         extra={<TooltipIcon content={<ReplaceKeplrTooltip />} />}
@@ -24,11 +28,11 @@ const AdvancedSettings = () => {
       />
       {replaceKeplr && (
         <FormWarning>
-          To prevent collisions between Keplr and Station, please uninstall or
-          disable the Keplr extension on your browser when this feature is
-          enabled.
+          To prevent collisions between Keplr and Station, when this fature is
+          enabled uninstall or disable the Keplr extension on your browser.
         </FormWarning>
       )}
+      */}
       <SettingsSelectorToggle
         onChange={changeDevMode}
         extra={<TooltipIcon content={<DevModeTooltip />} />}
