8b60e2572d44c83cafa5beb183dc30ba7fbadfd9
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-09-19 10:48:24 +0200

    Fix Ledger Modal for extension

--- src/components/feedback/modals/ModalButton.tsx
+++ src/components/feedback/modals/ModalButton.tsx
@@ -1,60 +1,62 @@
 /* eslint-disable react-refresh/only-export-components */
-import Modal, { ModalProps } from './Modal';
-import { createContext, useState, useEffect, ReactNode, useContext } from 'react';
+import Modal, { ModalProps } from "./Modal"
+import {
+  createContext,
+  useState,
+  useEffect,
+  ReactNode,
+  useContext,
+} from "react"
 
 /* helper */
 interface ModalContextValue {
-  openModal: () => void;
-  closeModal: () => void;
+  openModal: () => void
+  closeModal: () => void
 }
 
-const ModalContext = createContext<ModalContextValue | undefined>(undefined);
+const ModalContext = createContext<ModalContextValue | undefined>(undefined)
 
 export const useModal = (): ModalContextValue => {
-  const context = useContext(ModalContext);
+  const context = useContext(ModalContext)
 
   if (!context) {
-    throw new Error('useModal must be used within a ModalProvider');
+    throw new Error("useModal must be used within a ModalProvider")
   }
 
-  return context;
+  return context
 }
 
-export interface ModalButtonProps extends ModalProps {
-  renderButton: (openModal: () => void) => ReactNode;
-  minimal?: boolean;
-  children?: ReactNode;
-  isOpen?: boolean;
+export interface ModalButtonProps extends Omit<ModalProps, "isOpen"> {
+  renderButton: (openModal: () => void) => ReactNode
+  minimal?: boolean
+  children?: ReactNode
+  isOpen?: boolean
 }
 
 const ModalButton = (props: ModalButtonProps) => {
-  const { renderButton, isOpen, ...rest } = props;
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { renderButton, isOpen, ...rest } = props
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+  const openModal = () => setIsModalOpen(true)
+  const closeModal = () => setIsModalOpen(false)
 
   useEffect(() => {
-    if (isOpen) openModal();
-  }, [isOpen]);
+    if (isOpen) openModal()
+  }, [isOpen])
 
   const modalContextValue: ModalContextValue = {
     openModal: openModal,
     closeModal: closeModal,
-  };
+  }
 
   return (
     <ModalContext.Provider value={modalContextValue}>
       {renderButton(openModal)}
       {isModalOpen && (
-        <Modal
-          {...rest}
-          isOpen={isModalOpen}
-          onRequestClose={closeModal}
-        />
+        <Modal {...rest} isOpen={isModalOpen} onRequestClose={closeModal} />
       )}
     </ModalContext.Provider>
-  );
-};
+  )
+}
 
-export default ModalButton;
+export default ModalButton
--- src/components/ledger/LedgerModal.tsx
+++ src/components/ledger/LedgerModal.tsx
@@ -1,25 +1,25 @@
-import { PropsWithChildren } from 'react'
-import Lottie from 'lottie-react'
-import styles from './LedgerModal.module.scss'
-import { Modal } from 'components/feedback/modals'
-import animations from './animations'
+import { PropsWithChildren } from "react"
+import Lottie from "lottie-react"
+import styles from "./LedgerModal.module.scss"
+import { Modal } from "../feedback/modals"
+import animations from "./animations"
 
-enum LedgerModel {
-  NANOX = 'nanox',
-  NANOS = 'nanos',
-  NANOSP = 'nanosp',
+export enum LedgerDeviceModel {
+  NANOX = "nanox",
+  NANOS = "nanos",
+  NANOSP = "nanosp",
 }
 
-enum Action {
-  CONNECT = 'connect',
-  OPEN_APP = 'openApp',
-  CONFIRM = 'confirm',
-  UNLOCK = 'unlock',
+export enum LedgerDeviceAction {
+  CONNECT = "connect",
+  OPEN_APP = "openApp",
+  CONFIRM = "confirm",
+  UNLOCK = "unlock",
 }
 
 export interface LedgerModalProps {
-  device?: LedgerModel
-  action: Action
+  device?: LedgerDeviceModel
+  action: LedgerDeviceAction
   appName: string
   onRequestClose?: () => void
 }
@@ -27,21 +27,26 @@ export interface LedgerModalProps {
 interface Props extends LedgerModalProps {}
 
 const LedgerModal = (props: PropsWithChildren<Props>) => {
-  const { device = LedgerModel.NANOX, action, appName, onRequestClose } = props
+  const {
+    device = LedgerDeviceModel.NANOX,
+    action,
+    appName,
+    onRequestClose,
+  } = props
 
   function renderText() {
     switch (action) {
-      case Action.CONNECT:
+      case LedgerDeviceAction.CONNECT:
         return <p>Connect and unlock your Ledger device.</p>
-      case Action.UNLOCK:
+      case LedgerDeviceAction.UNLOCK:
         return <p>Turn on and unlock your Ledger device.</p>
-      case Action.OPEN_APP:
+      case LedgerDeviceAction.OPEN_APP:
         return (
           <p>
             Open the <b>{appName}</b> app on your Ledger device.
           </p>
         )
-      case Action.CONFIRM:
+      case LedgerDeviceAction.CONFIRM:
         return <p>Confirm the transaction on your Ledger device.</p>
     }
   }
