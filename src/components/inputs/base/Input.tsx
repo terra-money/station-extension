6b59cff2a6b03d6069cd73028e2bd8d3f8dd6a73
plubber <er.h.gorski@gmail.com>
2023-09-26 15:11:10 -0400

    revert: prev changes

--- src/components/inputs/base/Input.tsx
+++ src/components/inputs/base/Input.tsx
@@ -8,49 +8,45 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   actionIcon?: {
     icon: ReactNode
     onClick?: () => void
-  },
-  label?: string
+  }
   emoji?: string
 }
 
 const Input = forwardRef(
   (
-    { actionIcon, emoji, label, ...attrs }: InputProps,
+    { actionIcon, emoji, ...attrs }: InputProps,
     ref: ForwardedRef<HTMLInputElement>
   ) => {
     return (
-      <div className={cx(styles.container)}>
-        {label && <span>{label}</span>}
-        <div className={cx(styles.base__input)}>
-          <input
-            className={styles.base__input__field}
-            type={attrs.type ?? 'text'}
-            autoComplete='off'
-            autoFocus={attrs.autoFocus ?? false}
-            onWheel={(e) => e.currentTarget.blur()}
-            {...attrs}
-            ref={ref}
-          />
-          {actionIcon && (
-            <button
-              type='button'
-              className={styles.base__input__action}
-              onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                actionIcon?.onClick?.();
-              }}
-            >
-              {actionIcon.icon}
-            </button>
-          )}
+      <div className={cx(styles.base__input)}>
+        <input
+          className={styles.base__input__field}
+          type={attrs.type ?? 'text'}
+          autoComplete='off'
+          autoFocus={attrs.autoFocus ?? false}
+          onWheel={(e) => e.currentTarget.blur()}
+          {...attrs}
+          ref={ref}
+        />
+        {actionIcon && (
+          <button
+            type='button'
+            className={styles.base__input__action}
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              actionIcon?.onClick?.();
+            }}
+          >
+            {actionIcon.icon}
+          </button>
+        )}
 
-          {emoji && (
-            <span className={styles.base__input__emoji}>
-              {emoji}
-            </span>
-          )}
-        </div>
+        {emoji && (
+          <span className={styles.base__input__emoji}>
+            {emoji}
+          </span>
+        )}
       </div>
     );
   }
