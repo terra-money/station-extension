d16868866afa2e6971911d8b208385b78a88e569
plubber <er.h.gorski@gmail.com>
2023-10-03 16:03:35 -0400

    wip: recieve page

--- src/pages/wallet/ReceivePage.tsx
+++ src/pages/wallet/ReceivePage.tsx
@@ -1,14 +1,14 @@
 import { useTranslation } from "react-i18next"
 import styles from "./ReceivePage.module.scss"
 import { capitalize } from "@mui/material"
-import AddressTable from "app/components/AddressTable"
+import Addresses from "./Addresses"
 
 const ReceivePage = () => {
   const { t } = useTranslation()
   return (
     <section className={styles.receive}>
       <h1>{capitalize(t("receive"))}</h1>
-      <AddressTable />
+      {/* <Addresses /> */}
     </section>
   )
 }
