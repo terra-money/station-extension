7a1f5a6fce45508fac122abe5accacbc952c03b3
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-10-07 12:21:41 +0200

    Full screen extension design

--- src/extension/auth/RecoverWalletPage.tsx
+++ src/extension/auth/RecoverWalletPage.tsx
@@ -1,12 +1,18 @@
-import { useTranslation } from "react-i18next"
-import RecoverWalletForm from "auth/modules/create/RecoverWalletForm"
-import ExtensionPage from "../components/ExtensionPage"
+import { useTranslation } from 'react-i18next'
+import RecoverWalletForm from 'auth/modules/create/RecoverWalletForm'
+import ExtensionPage from '../components/ExtensionPage'
 
 const RecoverWallet = () => {
   const { t } = useTranslation()
 
   return (
-    <ExtensionPage title={t("Import Wallet")} backButtonPath="/" fullHeight>
+    <ExtensionPage
+      title={t('Import Wallet')}
+      subtitle={t(
+        "Enter your preferred wallet name and the wallet's seed phrase or private key.",
+      )}
+      fullHeight
+    >
       <RecoverWalletForm />
     </ExtensionPage>
   )
