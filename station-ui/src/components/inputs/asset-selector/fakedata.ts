27a51e31860ace9ded33ba014be03c696eccf882
Joshua <joshua.brigati@gmail.com>
2023-10-09 17:00:44 -0500

    updated the Dropdown component to have a children override and a search to filter options

--- station-ui/src/components/inputs/asset-selector/fakedata.ts
+++ station-ui/src/components/inputs/asset-selector/fakedata.ts
@@ -4,10 +4,10 @@ interface IWalletBalance {
 }
 
 const walletBalance: IWalletBalance = {
-  LUNA: '420.00',
-  UST: '0.00',
-  axlUSDC: '0.00',
-};
+  LUNA: "420.00",
+  UST: "0.00",
+  axlUSDC: "100.00",
+}
 
 interface ITokensBySymbol {
   [key: string]: {
@@ -15,37 +15,50 @@ interface ITokensBySymbol {
     tokenIcon: string;
     chainIcon: string;
     chainName: string;
-  };
+  }
 }
 
 const tokensBySymbol: ITokensBySymbol  = {
-  'LUNA': {
-    symbol: 'LUNA',
-    tokenIcon: 'https://station-assets.terra.dev/img/coins/Luna.svg',
-    chainIcon: 'https://station-assets.terra.dev/img/chains/Terra.svg',
-    chainName: 'Terra',
+  "LUNA": {
+    symbol: "LUNA",
+    tokenIcon: "https://station-assets.terra.dev/img/coins/Luna.svg",
+    chainIcon: "https://station-assets.terra.dev/img/chains/Terra.svg",
+    chainName: "Terra",
   },
-  'axlUSDC': {
-    symbol: 'axlUSDC',
-    tokenIcon: 'https://station-assets.terra.dev/img/coins/axlUSDC.svg',
-    chainIcon: 'https://station-assets.terra.dev/img/chains/Axelar.svg',
-    chainName: 'Axelar',
+  "axlUSDC": {
+    symbol: "axlUSDC",
+    tokenIcon: "https://station-assets.terra.dev/img/coins/axlUSDC.svg",
+    chainIcon: "https://station-assets.terra.dev/img/chains/Axelar.svg",
+    chainName: "Axelar",
   },
-  'OSMO': {
-    symbol: 'OSMO',
-    tokenIcon: 'https://station-assets.terra.dev/img/coins/Osmo.svg',
-    chainIcon: 'https://station-assets.terra.dev/img/chains/Cosmos.svg',
-    chainName: 'Cosmos',
+  "OSMO": {
+    symbol: "OSMO",
+    tokenIcon: "https://station-assets.terra.dev/img/coins/Osmo.svg",
+    chainIcon: "https://station-assets.terra.dev/img/chains/Cosmos.svg",
+    chainName: "Cosmos",
   },
-};
+  "OSMO2": {
+    symbol: "OSMO2",
+    tokenIcon: "https://station-assets.terra.dev/img/coins/Osmo.svg",
+    chainIcon: "https://station-assets.terra.dev/img/chains/Cosmos.svg",
+    chainName: "Cosmos",
+  },
+  "OSMO3": {
+    symbol: "OSMO3",
+    tokenIcon: "https://station-assets.terra.dev/img/coins/Osmo.svg",
+    chainIcon: "https://station-assets.terra.dev/img/chains/Cosmos.svg",
+    chainName: "Cosmos",
+  },
+}
+
 interface ITokenPrices {
   [key: string]: number;
 }
 
 const tokenPrices: ITokenPrices  = {
-  'LUNA': 0.39144,
-  'axlUSDC': 1,
-  'OSMO': 0.325555,
-};
+  "LUNA": 0.39144,
+  "axlUSDC": 1,
+  "OSMO": 0.325555,
+}
 
-export { walletBalance, tokensBySymbol, tokenPrices };
+export { walletBalance, tokensBySymbol, tokenPrices }
