43c689c6d60348f8daa4ddf0cc9fce5fd09e205e
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2023-10-13 09:04:15 +0300

    wip: activity hook

--- src/pages/activity/ActivityList.tsx
+++ src/pages/activity/ActivityList.tsx
@@ -11,6 +11,7 @@ import { useState } from "react"
 import { LoadingCircular } from "station-ui"
 import ActivityItem from "./ActivityItem"
 import styles from "./ActivityList.module.scss"
+import { useActivity } from "data/queries/activity"
 
 interface Props {
   chainID?: string
@@ -46,101 +47,22 @@ function getPaginationKeys(isTerra: boolean): PaginationKeys {
 
 const ActivityList = () => {
   const addresses = useInterchainAddresses()
-  const networks = useNetwork()
-
-  const LIMIT = 100
-  const EVENTS = [
-    // any tx signed by the user
-    "message.sender",
-    // any coin received
-    "transfer.recipient",
-    // any coin sent
-    "transfer.sender",
-  ]
-
-  const activityData = useQueries(
-    Object.keys(addresses ?? {}).map((chain) => {
-      const address = chain && addresses?.[chain]
-
-      const isTerra = isTerraChain(chain)
-
-      // return pagination keys by network.
-      const paginationKeys = getPaginationKeys(isTerra)
-
-      return {
-        queryKey: [queryKey.History, networks?.[chain]?.lcd, address],
-        queryFn: async () => {
-          const result: any[] = []
-          const hashArray: string[] = []
-
-          if (!networks?.[chain]?.lcd) {
-            return result
-          }
-
-          const requests = await Promise.all(
-            EVENTS.map((event) => {
-              return axios.get<AccountHistory>(`/cosmos/tx/v1beta1/txs`, {
-                baseURL: networks[chain].lcd,
-                params: {
-                  events: `${event}='${address}'`,
-                  //order_by: "ORDER_BY_DESC",
-                  [paginationKeys.offset]: 0 || undefined,
-                  [paginationKeys.reverse]: isTerra ? 2 : true,
-                  [paginationKeys.limit]: LIMIT,
-                },
-              })
-            })
-          )
-
-          for (const { data } of requests) {
-            data.tx_responses.forEach((tx) => {
-              if (!hashArray.includes(tx.txhash)) {
-                result.push(tx)
-                hashArray.push(tx.txhash)
-              }
-            })
-          }
-
-          return result
-            .sort((a, b) => Number(b.height) - Number(a.height))
-            .slice(0, LIMIT)
-            .map((tx) => ({ ...tx, chain }))
-        },
-        // Data will never become stale and always stay in cache
-        cacheTime: Infinity,
-        staleTime: Infinity,
-        refetchOnMount: false,
-        refetchOnWindowFocus: false,
-        refetchOnReconnect: false,
-        refetchIntervalInBackground: false,
-      }
-    })
-  )
-
-  const isLoading = activityData.some((query) => query.isLoading)
-
-  const state = combineState(...activityData)
-  const activitySorted = activityData
-    .reduce((acc, { data }) => (data ? [...acc, ...data] : acc), [] as any[])
-    .sort(
-      (a, b) =>
-        new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
-    )
-    .slice(0, LIMIT)
+  const { activitySorted: activity, state } = useActivity(addresses)
 
   const render = () => {
-    if (addresses && !activitySorted) return null
+    if (addresses && !activity) return null
 
-    return !activitySorted?.length ? (
+    return !activity?.length ? (
       <Card>
         <Empty />
       </Card>
     ) : (
       <div className={styles.activitylist}>
-        {!isLoading &&
-          activitySorted.map((item) => (
-            <ActivityItem {...item} key={item.txhash} />
-          ))}
+        {state.isLoading ? (
+          <LoadingCircular size={36} thickness={2} />
+        ) : (
+          activity.map((item) => <ActivityItem {...item} key={item.txhash} />)
+        )}
       </div>
     )
   }
