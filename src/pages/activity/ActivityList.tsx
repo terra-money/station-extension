a0a005059ee2f1fa0e590d05c6a3164025afc5bc
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2023-10-13 09:26:55 +0300

    wip: cleanup

--- src/pages/activity/ActivityList.tsx
+++ src/pages/activity/ActivityList.tsx
@@ -1,49 +1,10 @@
-import { Fragment } from "react"
-import { useQueries, useInfiniteQuery } from "react-query"
-import axios from "axios"
-import { combineState, queryKey } from "data/query"
-import { useNetwork } from "data/wallet"
-import { Card, Col, Page } from "components/layout"
+import { Card, Page } from "components/layout"
 import { Empty } from "components/feedback"
 import { useInterchainAddresses } from "auth/hooks/useAddress"
-import { isTerraChain } from "utils/chain"
-import { useState } from "react"
 import { LoadingCircular } from "station-ui"
 import ActivityItem from "./ActivityItem"
 import styles from "./ActivityList.module.scss"
 import { useActivity } from "data/queries/activity"
-
-interface Props {
-  chainID?: string
-}
-
-interface PaginationKeys {
-  limit: string
-  offset: string
-  reverse: string
-}
-
-/**
- * Returns pagination keys for the given chain. Switched by cosmos_sdk
- * version in the future, isTerra for now.
- *
- * @param isTerra boolean based on chain-id.  True if Terra, false if not.
- */
-function getPaginationKeys(isTerra: boolean): PaginationKeys {
-  if (isTerra) {
-    return {
-      limit: "limit",
-      offset: "page",
-      reverse: "orderBy",
-    }
-  } else {
-    return {
-      limit: "pagination.limit",
-      offset: "pagination.offset",
-      reverse: "pagination.reverse",
-    }
-  }
-}
 
 const ActivityList = () => {
   const addresses = useInterchainAddresses()
