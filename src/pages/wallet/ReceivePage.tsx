29ed9ad601d4004331e592f46517b9f3468d2d6b
plubber <er.h.gorski@gmail.com>
2023-10-16 11:13:38 -0400

    feat: add back button

--- src/pages/wallet/ReceivePage.tsx
+++ src/pages/wallet/ReceivePage.tsx
@@ -3,11 +3,12 @@ import { getChainNamefromID } from "data/queries/chains"
 import { useNetwork } from "data/wallet"
 import { useInterchainAddresses } from "auth/hooks/useAddress"
 import WithSearchInput from "pages/custom/WithSearchInput"
-import { AddressSelectableListItem } from "station-ui"
+import { AddressSelectableListItem, Button } from "station-ui"
 import { truncate } from "@terra-money/terra-utils"
 import { capitalize } from "@mui/material"
 import styles from "./ReceivePage.module.scss"
 import { useWalletRoute, Page } from "./Wallet"
+import { useTranslation } from "react-i18next"
 
 interface SearchChainsProps {
   data: {
@@ -18,10 +19,39 @@ interface SearchChainsProps {
   }[]
 }
 
+export const SearchChains = ({ data }: SearchChainsProps) => {
+  const networks = useNetwork()
+  return (
+    <>
+      <WithSearchInput label="Search Chains" className={styles.receive}>
+        {(input) =>
+          data
+            .filter((item) =>
+              item.name.toLowerCase().includes(input.toLowerCase())
+            )
+            .map(({ address, id, name, onClick }) => (
+              <AddressSelectableListItem
+                key={id}
+                label={capitalize(name)}
+                chain={{
+                  icon: networks[id].icon,
+                  label: name,
+                }}
+                subLabel={truncate(address)}
+                onClick={onClick}
+              />
+            ))
+        }
+      </WithSearchInput>
+    </>
+  )
+}
+
 const ReceivePage = () => {
   const addresses = useInterchainAddresses()
   const { setRoute } = useWalletRoute()
   const networks = useNetwork()
+  const { t } = useTranslation()
 
   const data = useMemo(() => {
     if (!addresses) return []
@@ -39,32 +69,17 @@ const ReceivePage = () => {
   }, [addresses, networks, setRoute])
 
   if (!data.length) return null
-  return <SearchChains data={data} />
-}
 
-export const SearchChains = ({ data }: SearchChainsProps) => {
-  const networks = useNetwork()
   return (
-    <WithSearchInput label="Search Chains" className={styles.receive}>
-      {(input) => {
-        return data
-          .filter((item) =>
-            item.name.toLowerCase().includes(input.toLowerCase())
-          )
-          .map(({ address, id, name, onClick }) => (
-            <AddressSelectableListItem
-              key={id}
-              label={capitalize(name)}
-              chain={{
-                icon: networks[id].icon,
-                label: name,
-              }}
-              subLabel={truncate(address)}
-              onClick={onClick}
-            />
-          ))
-      }}
-    </WithSearchInput>
+    <>
+      <SearchChains data={data} />
+      <Button
+        label={t("Back")}
+        className={styles.back}
+        onClick={() => setRoute({ page: Page.wallet })}
+        variant="secondary"
+      />
+    </>
   )
 }
 
