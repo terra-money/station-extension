1f88a3d18b0cdb1da1c1dba21ad5d3f544e5470b
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2023-10-16 19:37:08 +0300

    fix: ExtensionPage

--- station-extension/src/pages/multisig/SignMultisigTxPage.tsx
+++ station-extension/src/pages/multisig/SignMultisigTxPage.tsx
@@ -1,9 +1,10 @@
+import ExtensionPage from "extension/components/ExtensionPage"
 import useDefaultValues from "./utils/useDefaultValues"
 import SignMultisigTxForm from "./SignMultisigTxForm"
 import { useTranslation } from "react-i18next"
-import { Card, Page } from "components/layout"
 import { Wrong } from "components/feedback"
 import { isWallet, useAuth } from "auth"
+import { Card } from "components/layout"
 
 const SignMultisigTxPage = () => {
   const { t } = useTranslation()
@@ -22,9 +23,9 @@ const SignMultisigTxPage = () => {
   }
 
   return (
-    <Page title={t("Sign Multisig Tx")} backButtonPath="/">
+    <ExtensionPage title={t("Sign Multisig Tx")} backButtonPath="/">
       {render()}
-    </Page>
+    </ExtensionPage>
   )
 }
 
