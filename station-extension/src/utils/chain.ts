2874a7fc4a9688be798ea728d85fac64350b78e8
plubber <er.h.gorski@gmail.com>
2023-10-25 13:12:15 -0400

    cleanup

--- station-extension/src/utils/chain.ts
+++ station-extension/src/utils/chain.ts
@@ -1,5 +1,4 @@
 import { AccAddress } from "@terra-money/feather.js"
-import { bech32 } from "bech32"
 
 export const isTerraChain = (chainID: string) => {
   return chainID.startsWith("phoenix-") || chainID.startsWith("pisco-")
@@ -10,13 +9,3 @@ export const isNativeToken = (denom: string) =>
   !denom.startsWith("factory/") &&
   !denom.startsWith("gamm/") &&
   !AccAddress.validate(denom)
-
-export const convertAddress = (address: AccAddress, newPrefix: string) => {
-  let decodedAddress
-  try {
-    decodedAddress = bech32.decode(address)
-  } catch (error) {
-    throw new Error("Provided address is not a valid Bech32 address.")
-  }
-  return bech32.encode(newPrefix, decodedAddress.words)
-}
