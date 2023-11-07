43330753bce45b6690016e2c3446edb1a83a8714
plubber <er.h.gorski@gmail.com>
2023-11-07 17:48:28 -0500

    feat: swap form context setup

--- station-extension/src/txs/swap/SwapForm.tsx
+++ station-extension/src/txs/swap/SwapForm.tsx
@@ -2,22 +2,33 @@ import { AssetSelectorTo, AssetSelectorFrom, Form } from "station-ui"
 import { useSwapTokens } from "data/queries/swap/hook"
 import { SwapState } from "data/queries/swap/types"
 import { useForm } from "react-hook-form"
+import { useSwap } from "./SwapContext"
 
 const SwapForm = () => {
-  const tokens = useSwapTokens()
+  const { tokens } = useSwap()
   const { watch, register } = useForm<SwapState>({ mode: "onChange" })
   const { offerAsset, askAsset } = watch()
   console.log("tokens", tokens)
   const asset = offerAsset ?? tokens[0]
-  if (!tokens.length) return null
+  // if (!tokens.length) return null
 
   return (
     <Form>
       <AssetSelectorFrom
         extra={asset.balance}
         symbol={asset.symbol}
-        chainIcon={asset.chain.icon}
-        chainName={asset.chain.name}
+        chainIcon={asset.chain?.icon ?? ""}
+        chainName={asset.chain?.name ?? ""}
+        tokenIcon={asset.icon ?? ""}
+        onSymbolClick={() => {}}
+        currencyAmount={asset.value.toString()}
+        amountInputAttrs={{ ...register("offerAsset.amount") }}
+      />
+      <AssetSelectorFrom
+        extra={asset.balance}
+        symbol={asset.symbol}
+        chainIcon={asset.chain?.icon ?? ""}
+        chainName={asset.chain?.name ?? ""}
         tokenIcon={asset.icon ?? ""}
         onSymbolClick={() => {}}
         currencyAmount={asset.value.toString()}
