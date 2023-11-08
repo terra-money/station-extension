6a26397197be48432269783a9842aacdb19251a5
plubber <er.h.gorski@gmail.com>
2023-11-08 10:29:41 -0500

    wip: swap page implementation

--- src/txs/swap/SwapForm.tsx
+++ src/txs/swap/SwapForm.tsx
@@ -1,39 +1,50 @@
-import { AssetSelectorTo, AssetSelectorFrom, Form } from "station-ui"
-import { useSwapTokens } from "data/queries/swap/hook"
+import {
+  AssetSelectorTo,
+  AssetSelectorFrom,
+  Form,
+  SubmitButton,
+} from "station-ui"
 import { SwapState } from "data/queries/swap/types"
 import { useForm } from "react-hook-form"
 import { useSwap } from "./SwapContext"
+import { useTranslation } from "react-i18next"
+import { toInput } from "txs/utils"
 
 const SwapForm = () => {
   const { tokens } = useSwap()
-  const { watch, register } = useForm<SwapState>({ mode: "onChange" })
+  const defaultOfferAsset =
+    tokens.find((token) => token.balance > 0) ?? tokens[0]
+  const defaultAskAsset = tokens[1]
+  const { watch, register, handleSubmit, setValue } = useForm<SwapState>({
+    mode: "onChange",
+    defaultValues: { offerAsset: defaultOfferAsset, askAsset: defaultAskAsset },
+  })
   const { offerAsset, askAsset } = watch()
-  console.log("tokens", tokens)
-  const asset = offerAsset ?? tokens[0]
-  // if (!tokens.length) return null
+  const { t } = useTranslation()
 
   return (
     <Form>
       <AssetSelectorFrom
-        extra={asset.balance}
-        symbol={asset.symbol}
-        chainIcon={asset.chain?.icon ?? ""}
-        chainName={asset.chain?.name ?? ""}
-        tokenIcon={asset.icon ?? ""}
+        extra={toInput(offerAsset?.balance, offerAsset?.decimals)}
+        symbol={offerAsset?.symbol}
+        chainIcon={offerAsset?.chain?.icon}
+        chainName={offerAsset?.chain?.name}
+        tokenIcon={offerAsset?.icon ?? ""}
         onSymbolClick={() => {}}
-        currencyAmount={asset.value.toString()}
+        currencyAmount={offerAsset?.value.toString()}
         amountInputAttrs={{ ...register("offerAmount") }}
       />
-      <AssetSelectorFrom
-        extra={asset.balance}
-        symbol={asset.symbol}
-        chainIcon={asset.chain?.icon ?? ""}
-        chainName={asset.chain?.name ?? ""}
-        tokenIcon={asset.icon ?? ""}
+      <AssetSelectorTo
+        extra={askAsset?.balance}
+        symbol={askAsset?.symbol}
+        chainIcon={askAsset?.chain?.icon ?? ""}
+        chainName={askAsset.chain?.name ?? ""}
+        tokenIcon={askAsset.icon ?? ""}
         onSymbolClick={() => {}}
-        currencyAmount={asset.value.toString()}
-        amountInputAttrs={{}}
+        amount={"Simulating..."}
+        currencyAmount={askAsset.value.toString()}
       />
+      <SubmitButton label={t("Swap")} />
     </Form>
   )
 }
