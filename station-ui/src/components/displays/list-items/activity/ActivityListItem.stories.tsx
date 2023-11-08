c6b598cf98a3bd6acdffec5594d5f2466c63cb14
Joshua <joshua.brigati@gmail.com>
2023-11-07 23:59:34 -0600

    updated ActivityListItem and Timeline to match new designs

--- station-ui/src/components/displays/list-items/activity/ActivityListItem.stories.tsx
+++ station-ui/src/components/displays/list-items/activity/ActivityListItem.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import ActivityListItem, { ActivityListItemProps } from './ActivityListItem';
+import { Pill, ProgressTracker } from 'components';
 
 const meta: Meta<ActivityListItemProps> = {
   title: 'Components/List Items/Activity/Stories',
@@ -21,7 +22,7 @@ export const Successful: StoryObj<ActivityListItemProps> = {
           </div>
         }
         type={"Execute Contract"}
-        time={"2 minutes ago"}
+        msgCount={1}
       />
     )
   },
@@ -66,6 +67,70 @@ export const Loading: StoryObj<ActivityListItemProps> = {
   argTypes: {},
 };
 
+export const NoStatusIcon: StoryObj<ActivityListItemProps> = {
+  render: () => {
+    return (
+      <ActivityListItem
+        chain={{ icon: "https://station-assets.terra.dev/img/chains/Terra.svg", label: "Terra" }}
+        msg={
+          <div>
+            Sent <span>420.00 LUNA</span> to <span>terra1...20k38v</span>
+          </div>
+        }
+        type={"Execute Contract"}
+        msgCount={3}
+      />
+    )
+  },
+  argTypes: {},
+};
+
+export const SecondaryPill: StoryObj<ActivityListItemProps> = {
+  render: () => {
+    return (
+      <ActivityListItem
+        variant={"loading"}
+        chain={{ icon: "https://station-assets.terra.dev/img/chains/Terra.svg", label: "Terra" }}
+        msg={
+          <div>
+            Sent <span>420.00 LUNA</span> to <span>terra1...20k38v</span>
+          </div>
+        }
+        type={"Execute Contract"}
+        msgCount={1}
+        secondaryPill={<Pill variant={"warning"} text={"Pending"} />}
+      />
+    )
+  },
+  argTypes: {},
+};
+
+export const WithProgressTracker: StoryObj<ActivityListItemProps> = {
+  render: () => {
+    return (
+      <ActivityListItem
+        variant={"loading"}
+        chain={{ icon: "https://station-assets.terra.dev/img/chains/Terra.svg", label: "Terra" }}
+        msg={
+          <div>
+            Sent <span>420.00 LUNA</span> to <span>terra1...20k38v</span>
+          </div>
+        }
+        type={"Execute Contract"}
+        msgCount={1}
+        secondaryPill={<Pill variant={"warning"} text={"Pending"} />}
+        progressTracker={
+          <ProgressTracker
+            steps={["completed", "incomplete", "incomplete"]}
+            stepLabels={["Terra", "Osmosis", "Something"]}
+          />
+        }
+      />
+    )
+  },
+  argTypes: {},
+};
+
 export const Timeline: StoryObj<ActivityListItemProps> = {
   render: () => {
     return (
@@ -74,22 +139,12 @@ export const Timeline: StoryObj<ActivityListItemProps> = {
         chain={{ icon: "https://station-assets.terra.dev/img/chains/Terra.svg", label: "Terra" }}
         msg={
           <div>
-            Delegated <span>420.00 LUNA</span> to <span>Orbital Command</span>
+            Swapped <span>1,500 axlUSDC</span> for <span style={{color: "var(--token-success-500)"}}>3,422.65 LUNA</span> on <span>TFM</span>
           </div>
         }
         type={"Delegate"}
-        time={"1 day ago"}
-        timelineMessages={[
-          <div>
-            Delegated <span>420.00 LUNA</span> to <span>Orbital Command</span>
-          </div>,
-          <div>
-            Delegated <span>420.00 LUNA</span> to <span>Orbital Command</span>
-          </div>,
-          <div>
-            Delegated <span>420.00 LUNA</span> to <span>Orbital Command</span>
-          </div>,
-        ]}
+        msgCount={8}
+        hasTimeline={true}
       />
     )
   },
