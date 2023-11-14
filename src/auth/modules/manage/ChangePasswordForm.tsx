e3689a41079c620653da85419223288f6e061bbc
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-11-14 14:00:55 +0100

    Fix change password form

--- src/auth/modules/manage/ChangePasswordForm.tsx
+++ src/auth/modules/manage/ChangePasswordForm.tsx
@@ -2,17 +2,24 @@ import { useState } from "react"
 import { useTranslation } from "react-i18next"
 import { useNavigate } from "react-router-dom"
 import { useForm } from "react-hook-form"
-// import { Input } from "components/form"
 import {
   changePassword,
+  isPasswordValid,
   setShouldStorePassword,
   shouldStorePassword,
   storePassword,
 } from "../../scripts/keystore"
 import validate from "auth/scripts/validate"
-import useAuth from "../../hooks/useAuth"
-import ConfirmModal from "./ConfirmModal"
-import { Form, InputWrapper, SubmitButton, Input, Checkbox } from "station-ui"
+import {
+  Form,
+  InputWrapper,
+  SubmitButton,
+  Input,
+  Checkbox,
+  SummaryHeader,
+  Grid,
+  Button,
+} from "station-ui"
 
 interface Values {
   current: string
@@ -24,19 +31,27 @@ interface Values {
 const ChangePasswordForm = () => {
   const { t } = useTranslation()
   const navigate = useNavigate()
-  const { validatePassword } = useAuth()
 
   /* form */
   const form = useForm<Values>({
     mode: "onChange",
     defaultValues: { rememberPassword: shouldStorePassword() },
   })
-  const { register, watch, handleSubmit, formState } = form
+  const { register, watch, handleSubmit, formState, setError } = form
   const { errors } = formState
-  const { password, rememberPassword } = watch()
+  const { current, password, rememberPassword, confirm } = watch()
 
   const [done, setDone] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
   const submit = ({ current, password, rememberPassword }: Values) => {
+    setIsLoading(true)
+    if (!isPasswordValid(current)) {
+      setError("current", { message: t("Invalid password") })
+      setIsLoading(false)
+      return
+    }
+
     changePassword({ oldPassword: current, newPassword: password })
     if (rememberPassword) {
       setShouldStorePassword(true)
@@ -44,64 +59,72 @@ const ChangePasswordForm = () => {
     } else {
       setShouldStorePassword(false)
     }
+    setIsLoading(false)
     setDone(true)
   }
 
-  return (
-    <>
-      {done && (
-        <ConfirmModal onRequestClose={() => navigate("/")}>
-          {t("Password changed successfully")}
-        </ConfirmModal>
-      )}
-
-      <Form onSubmit={handleSubmit(submit)}>
-        <InputWrapper
-          label={t("Current password")}
-          error={errors.current?.message}
-        >
-          <Input
-            {...register("current", { validate: validatePassword })}
-            type="password"
-            autoFocus
-          />
-        </InputWrapper>
-
-        <InputWrapper
-          label={t("New password")}
-          error={errors.password?.message}
-        >
-          <Input
-            {...register("password", { validate: validate.password })}
-            type="password"
-          />
-        </InputWrapper>
-
-        <InputWrapper
-          label={t("Confirm new password")}
-          error={errors.confirm?.message}
-        >
-          <Input
-            {...register("confirm", {
-              validate: (value) => validate.confirm(password, value),
-            })}
-            onFocus={() => form.trigger("confirm")}
-            type="password"
-          />
-        </InputWrapper>
-        <InputWrapper>
-          <Checkbox
-            label={t("Don't ask for password again")}
-            checked={rememberPassword}
-            {...register("rememberPassword")}
-          />
-        </InputWrapper>
-        <SubmitButton
-          label={t("Submit")}
-          disabled={!!Object.values(errors).length}
+  if (done) {
+    return (
+      <Grid gap={24}>
+        <SummaryHeader
+          statusLabel={t("Success!")}
+          statusMessage={t("Password changed successfully")}
+          status={"success"}
         />
-      </Form>
-    </>
+        <Button
+          variant="primary"
+          onClick={() => navigate("/preferences/security")}
+        >
+          {t("Done")}
+        </Button>
+      </Grid>
+    )
+  }
+
+  return (
+    <Form onSubmit={handleSubmit(submit)}>
+      <InputWrapper
+        label={t("Current password")}
+        error={errors.current?.message}
+      >
+        <Input {...register("current")} type="password" />
+      </InputWrapper>
+
+      <InputWrapper label={t("New password")} error={errors.password?.message}>
+        <Input
+          {...register("password", { validate: validate.password })}
+          type="password"
+        />
+      </InputWrapper>
+
+      <InputWrapper
+        label={t("Confirm new password")}
+        error={errors.confirm?.message}
+      >
+        <Input
+          {...register("confirm", {
+            validate: (value) => validate.confirm(password, value),
+          })}
+          type="password"
+        />
+      </InputWrapper>
+      <InputWrapper>
+        <Checkbox
+          label={t("Don't ask for password again")}
+          checked={rememberPassword}
+          {...register("rememberPassword")}
+        />
+      </InputWrapper>
+      <SubmitButton
+        label={t("Submit")}
+        disabled={
+          isLoading ||
+          !!Object.values(errors).find(({ message }) => !!message) ||
+          !(current && password && confirm)
+        }
+        loading={isLoading}
+      />
+    </Form>
   )
 }
 
