a39ff97c980a781506adb33c1238b9a3abcdfe86
Michael Merz <mwmerz@protonmail.com>
2023-11-15 16:42:48 -0500

    wip: swap works!

--- station-extension/src/txs/swap/Confirm.tsx
+++ station-extension/src/txs/swap/Confirm.tsx
@@ -1,9 +1,10 @@
+import { useMemo } from "react"
+import { MsgTransfer, Coin } from "@terra-money/feather.js"
 import { Form, SectionHeader, SummaryTable } from "station-ui"
 import { useLocation } from "react-router-dom"
 import { useTranslation } from "react-i18next"
 import SwapTimeline from "./components/SwapTimeline"
 import { toInput } from "txs/utils"
-import { useQuery } from "react-query"
 import { SwapState } from "data/queries/swap/types"
 import { toAmount } from "@terra-money/terra-utils"
 import Tx from "txs/Tx"
@@ -20,22 +21,39 @@ const Confirm = () => {
   const { state: swapState } = useLocation()
   const { t } = useTranslation()
   const { form } = useSwap()
-  const { watch, handleSubmit } = form
-  const { route, askAsset, offerAsset, msgs, offerInput } = watch()
+  const { watch, handleSubmit, getValues } = form
+  const { route, askAsset, offerAsset, msgs: formMsgs, offerInput } = watch()
 
   const amount = toAmount(offerInput, { decimals: offerAsset.decimals })
 
+  const createTx = ({ offerAsset }: SwapState) => {
+    const msg = JSON.parse(formMsgs[0][0]?.msg)
+
+    const msgs = [
+      new MsgTransfer(
+        "transfer",
+        msg.source_channel,
+        new Coin(msg.token?.denom ?? "", msg.token?.amount),
+        msg.sender,
+        msg.receiver,
+        undefined,
+        (Date.now() + 120 * 1000) * 1e6,
+        msg.memo
+      ),
+    ]
+
+    return { msgs, chainID: offerAsset.chainId }
+  }
+
+  /* fee */
+  const estimationTxValues = useMemo(() => getValues(), [getValues])
   const tx = {
     token: offerAsset.denom,
     decimals: offerAsset.decimals,
     amount,
     balance: offerAsset.balance,
-    createTx: () => {
-      return {
-        msgs,
-        chainID: offerAsset.chainId,
-      }
-    },
+    estimationTxValues,
+    createTx,
     // queryKeys: [offerAsset, askAsset]
     //   .filter((asset) => asset && AccAddress.validate(asset))
     //   .map((token) => [
@@ -50,7 +68,7 @@ const Confirm = () => {
   return (
     <Tx {...tx}>
       {({ fee, submit }) => (
-        <Form onSubmit={handleSubmit(() => {})}>
+        <Form onSubmit={handleSubmit(submit.fn)}>
           <SwapTimeline {...swapState} />
           <SectionHeader title={t("Details")} withLine />
           <SummaryTable
