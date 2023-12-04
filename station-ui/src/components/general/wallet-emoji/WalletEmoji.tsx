867f609177e0314ef60183a67f68d08ea1579065
plubber <er.h.gorski@gmail.com>
2023-12-04 12:08:02 -0500

    wip: my addresses view

--- station-ui/src/components/general/wallet-emoji/WalletEmoji.tsx
+++ station-ui/src/components/general/wallet-emoji/WalletEmoji.tsx
@@ -1,27 +1,37 @@
-import styles from "./WalletEmoji.module.scss"
+import styles from "./WalletEmoji.module.scss";
 
 export interface WalletEmojiProps {
-  id: string // walletName or an emoji
+  id: string; // wallet name, imgUrl or an emoji
 }
 
-const WalletEmoji = ({
-  id,
-}: WalletEmojiProps) => {
-  const emoji = id.match(/([\uD800-\uDBFF][\uDC00-\uDFFF])/g)
+const WalletEmoji = ({ id }: WalletEmojiProps) => {
+  const emoji = id.match(/([\uD800-\uDBFF][\uDC00-\uDFFF])/g);
+  let url;
 
-  return (
-    emoji ? (
-      <div className={styles.emoji}>
+  try {
+    url = new URL(id);
+  } catch (e) {
+    url = null;
+  }
+
+  if (emoji) return (
+    <div className={styles.emoji}>
         <span role='img' aria-label='emoji'>
-          {id}
+          { url?.href ? <img src={url.href} /> : id}
         </span>
       </div>
-    ) : (
-      <span className={styles.big__letter}>
-        {id.charAt(0).toUpperCase()}
-      </span>
-    )
   )
-}
 
-export default WalletEmoji
+  if (url?.href) return (
+    <img src={url.href} className={styles.img} />
+  )
+
+  return (
+    <span className={styles.big__letter}>
+        {id.charAt(0).toUpperCase()}
+    </span>
+  )
+
+};
+
+export default WalletEmoji;
