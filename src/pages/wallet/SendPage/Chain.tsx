9e20837b94a45d3524173d726c76d3b47666f586
plubber <er.h.gorski@gmail.com>
2023-12-07 14:33:03 -0500

    wip: coin type wallet derivation

--- src/pages/wallet/SendPage/Chain.tsx
+++ src/pages/wallet/SendPage/Chain.tsx
@@ -4,12 +4,16 @@ import { convertAddress } from "utils/chain"
 import { useNetwork } from "data/wallet"
 import { getChainNamefromID } from "data/queries/chains"
 import { SearchChains } from "../ReceivePage"
+import { addressFromWords } from "utils/bech32"
+import { useAuth } from "auth"
 
 const Chain = () => {
   const { form, goToStep, balances } = useSend()
   const { setValue } = form
   const networks = useNetwork()
   const { recipient } = form.watch()
+  const { wallets } = useAuth()
+  const wallet = wallets.find((w) => w.name === walletName)
 
   const availableChains = useMemo(() => {
     const chainsSet = new Set()
@@ -19,20 +23,27 @@ const Chain = () => {
 
   const chains = useMemo(
     () =>
-      availableChains.map((chain) => {
-        const address = convertAddress(recipient ?? "", networks[chain]?.prefix)
-        const name = getChainNamefromID(chain, networks) ?? chain
-        return {
-          name,
-          onClick: () => {
-            setValue("destination", chain)
-            setValue("recipient", address)
-            goToStep(3)
-          },
-          id: chain,
-          address,
-        }
-      }),
+      availableChains
+        .map((chain) => {
+          // only show chaisn where cointype available
+          if (!("words" in wallet)) return null
+          const address = addressFromWords(
+            wallet.words[networks[chain]?.coinType],
+            networks[chain]?.prefix
+          )
+          const name = getChainNamefromID(chain, networks) ?? chain
+          return {
+            name,
+            onClick: () => {
+              setValue("destination", chain)
+              setValue("recipient", address)
+              goToStep(3)
+            },
+            id: chain,
+            address,
+          }
+        })
+        .filter(Boolean),
     [availableChains, networks, recipient, setValue, goToStep]
   )
   return <SearchChains data={chains} />
