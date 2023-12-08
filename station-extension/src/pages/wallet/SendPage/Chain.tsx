0196d55ac77fd508e0a235afc38ede9c84b8b242
plubber <er.h.gorski@gmail.com>
2023-12-08 15:32:27 -0500

    feat: derive addresses from coin type if local wallet selected

--- station-extension/src/pages/wallet/SendPage/Chain.tsx
+++ station-extension/src/pages/wallet/SendPage/Chain.tsx
@@ -1,19 +1,17 @@
 import { useMemo } from "react"
 import { useSend } from "./SendContext"
-import { convertAddress } from "utils/chain"
 import { useNetwork } from "data/wallet"
 import { getChainNamefromID } from "data/queries/chains"
 import { SearchChains } from "../ReceivePage"
 import { addressFromWords } from "utils/bech32"
-import { useAuth } from "auth"
+import { AccAddress } from "@terra-money/feather.js"
+import { getWallet } from "auth/scripts/keystore"
 
 const Chain = () => {
   const { form, goToStep, balances } = useSend()
   const { setValue } = form
   const networks = useNetwork()
   const { recipient } = form.watch()
-  const { wallets } = useAuth()
-  const wallet = wallets.find((w) => w.name === walletName)
 
   const availableChains = useMemo(() => {
     const chainsSet = new Set()
@@ -21,31 +19,28 @@ const Chain = () => {
     return Array.from(chainsSet) as string[]
   }, [balances])
 
-  const chains = useMemo(
-    () =>
-      availableChains
-        .map((chain) => {
-          // only show chaisn where cointype available
-          if (!("words" in wallet)) return null
-          const address = addressFromWords(
-            wallet.words[networks[chain]?.coinType],
-            networks[chain]?.prefix
-          )
-          const name = getChainNamefromID(chain, networks) ?? chain
-          return {
-            name,
-            onClick: () => {
-              setValue("destination", chain)
-              setValue("recipient", address)
-              goToStep(3)
-            },
-            id: chain,
-            address,
-          }
-        })
-        .filter(Boolean),
-    [availableChains, networks, recipient, setValue, goToStep]
+  const chains = useMemo(() => {
+    const { words } = getWallet(recipient)
+    return availableChains.map((chain) => {
+      const { coinType, prefix } = networks[chain]
+      const address = addressFromWords(words[coinType], prefix)
+      const name = getChainNamefromID(chain, networks) ?? chain
+      return {
+        name,
+        onClick: () => {
+          setValue("destination", chain)
+          setValue("recipient", address)
+          goToStep(3)
+        },
+        id: chain,
+        address,
+      }
+    })
+  }, [availableChains, networks, recipient, setValue, goToStep])
+  return (
+    <SearchChains
+      data={chains.filter((item) => AccAddress.validate(item.address))}
+    />
   )
-  return <SearchChains data={chains} />
 }
 export default Chain
