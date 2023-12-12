252e946e5b8188fb7c2cb5091a4c8e0ce5cd78b0
plubber <er.h.gorski@gmail.com>
2023-12-12 15:35:04 -0500

    feat: new send flow and component updates + terra-utils  dep removal

--- src/pages/wallet/SendPage/Components/MyWallets.tsx
+++ src/pages/wallet/SendPage/Components/MyWallets.tsx
@@ -1,90 +1,31 @@
 import { useAddressBook } from "data/settings/AddressBook"
-import { getChainIdFromAddress } from "data/queries/chains"
-import { ReactNode } from "react"
-import { Grid, SectionHeader, WalletButton } from "@terra-money/station-ui"
-import { useNetwork } from "data/wallet"
-import { truncate } from "@terra-money/terra-utils"
+import { AddressBookList } from "./AddressBookList"
+import { LocalWalletList } from "./LocalWalletList"
+
 import { useAuth } from "auth"
 import { ReactComponent as FavoriteIcon } from "styles/images/icons/Favorite.svg"
 import { ReactComponent as ActiveWalletIcon } from "styles/images/icons/ActiveWallet.svg"
-import { useTranslation } from "react-i18next"
 
-interface Props {
+interface MyWalletsProps {
   onClick?: (address: string, index: number) => void
   tab: string
 }
-interface ButtonListItem {
-  name: string
-  icon?: string
-  recipient?: string
-}
 
-export const WalletButtonList = ({
-  items,
-  title,
-  icon,
-  onClick,
-  variant = "primary",
-  isMultipleAddresses = false,
-}: {
-  items: ButtonListItem[]
-  title?: string
-  icon?: ReactNode
-  variant?: "primary" | "secondary"
-  isMultipleAddresses?: boolean
-  onClick?: (address: string, index: number) => void
-}) => {
-  const network = useNetwork()
-  const { t } = useTranslation()
-  if (!items.length) return null
-  return (
-    <Grid gap={8}>
-      {title && <SectionHeader icon={icon} indented title={t(title)} />}
-      {items.map((i, index) => (
-        <WalletButton
-          variant={variant}
-          key={i.name}
-          emoji={i.icon}
-          walletName={i.name}
-          walletAddress={
-            isMultipleAddresses
-              ? t("Multiple Addresses")
-              : truncate(i.recipient)
-          }
-          chainIcon={
-            i.recipient
-              ? network[getChainIdFromAddress(i.recipient, network)]?.icon
-              : undefined
-          }
-          onClick={() => onClick?.(i.recipient ?? i.name, index)}
-        />
-      ))}
-    </Grid>
-  )
-}
-
-const MyWallets = ({ tab, onClick }: Props) => {
+const MyWallets = ({ tab, onClick }: MyWalletsProps) => {
   const { list: addressList } = useAddressBook()
   const { wallets, connectedWallet } = useAuth()
-
-  const localWallets: ButtonListItem[] = wallets.map((w) => ({
-    name: w.name,
-    icon: w.icon,
-  }))
 
   return (
     <>
       {tab === "address" ? (
         <>
-          <WalletButtonList
+          <AddressBookList
             onClick={onClick}
-            variant="secondary"
             title="Favorites"
             icon={<FavoriteIcon />}
             items={addressList.filter((i) => i.favorite)}
           />
-          <WalletButtonList
-            variant="secondary"
+          <AddressBookList
             title="All Addresses"
             onClick={onClick}
             items={addressList.filter((i) => !i.favorite)}
@@ -93,19 +34,17 @@ const MyWallets = ({ tab, onClick }: Props) => {
       ) : (
         <>
           {connectedWallet && (
-            <WalletButtonList
+            <LocalWalletList
               onClick={onClick}
               title="Active"
               icon={<ActiveWalletIcon />}
               items={[connectedWallet]}
-              isMultipleAddresses={true}
             />
           )}
-          <WalletButtonList
-            items={localWallets.filter((w) => w.name !== connectedWallet?.name)}
+          <LocalWalletList
+            items={wallets.filter((w) => w.name !== connectedWallet?.name)}
             onClick={onClick}
             title="Other Wallets"
-            isMultipleAddresses={true}
           />
         </>
       )}
