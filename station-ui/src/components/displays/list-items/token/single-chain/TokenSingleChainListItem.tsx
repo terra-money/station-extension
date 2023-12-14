b937508e5dfe768e8aa36bd8542428b0e5436d4d
Joshua <joshua.brigati@gmail.com>
2023-12-14 16:52:08 -0600

    added truncate back in to symbol

--- station-ui/src/components/displays/list-items/token/single-chain/TokenSingleChainListItem.tsx
+++ station-ui/src/components/displays/list-items/token/single-chain/TokenSingleChainListItem.tsx
@@ -35,7 +35,7 @@ const TokenSingleChainListItem = ({
         <div className={styles.details__container}>
           <div className={styles.top__row}>
             <h3 className={styles.symbol}>
-              <span className={styles.symbol__name}>{symbol}</span>
+              <span className={styles.symbol__name}>{truncate(symbol, [15, 15])}</span>
               {isSendBack && <Pill variant="warning" text="Send Back" />}
             </h3>
             <h3 className={styles.amount}>
