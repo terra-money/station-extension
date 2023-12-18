426059264018d94db7f9a1dd461df030317facd9
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2023-12-18 14:21:56 -0700

    feat: extract msg type
    Added regex to extract msg type if not available.

--- terra-utils/src/message.ts
+++ terra-utils/src/message.ts
@@ -37,15 +37,13 @@ export const getCanonicalMsg = (
   for (const [msgIdx, msg] of msgs.entries()) {
     const msgEvents = events.length ? events[msgIdx] : []
 
-    if (
-      (msg as any).from_address &&
-      (msg as any).to_address &&
-      (msg as any).amount
-    ) {
-      ;(msg as any)["@type"] = "/cosmos.bank.v1beta1.MsgSend"
-    }
+    // Adds `@type` key if not already in msg object.
+    const typePattern = /"@type\\"\s*:\s*\\"([^"]+)\\"/
+    const msgType = (msg as any)["@type"]
+      ? (msg as any)["@type"]
+      : (JSON.stringify(msg).match(typePattern) || [])[1] || null
 
-    switch ((msg as any)["@type"]) {
+    switch (msgType) {
       /* ---------------------------------- Send ---------------------------------- */
 
       case "/cosmos.bank.v1beta1.MsgSend":
@@ -328,7 +326,7 @@ export const getCanonicalMsg = (
       default:
         returnMsgs.push({
           msgType: "Unknown",
-          canonicalMsg: [(msg as any)["@type"]],
+          canonicalMsg: [msgType],
         })
         break
     }
