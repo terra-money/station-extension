0e0f837bf4b655d00a79406dc5411553e2fac5bb
plubber <er.h.gorski@gmail.com>
2023-12-18 13:18:08 -0500

    Update wallet names in WalletList component

--- station-ui/src/components/displays/wallet-list/WalletList.tsx
+++ station-ui/src/components/displays/wallet-list/WalletList.tsx
@@ -30,7 +30,7 @@ const WalletList = ({ activeWallet, otherWallets }: WalletListProps) => {
           />
           <WalletSelectableListItem
             label={activeWallet.name}
-            walletName="Big Money"
+            walletName={activeWallet.name}
             copyValue={activeWallet.address}
             subLabel={truncate(activeWallet.address)}
             active
@@ -46,7 +46,7 @@ const WalletList = ({ activeWallet, otherWallets }: WalletListProps) => {
           {otherWallets.map((wallet) => (
             <WalletSelectableListItem
               label={wallet.name}
-              walletName="Money"
+              walletName={wallet.name}
               copyValue={wallet.address}
               subLabel={truncate(wallet.address)}
               onClick={wallet.onClick}
