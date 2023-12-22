f9027af477c4c20ecfdd6613795ec0c752b816c5
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-12-22 11:58:19 +0100

    clenup unused variables

--- station-extension/src/extension/auth/SelectWalletsPage.tsx
+++ station-extension/src/extension/auth/SelectWalletsPage.tsx
@@ -3,7 +3,6 @@ import { useTranslation } from "react-i18next"
 import { FlexColumn, NavButton, WalletList } from "@terra-money/station-ui"
 import styles from "./SelectWallets.module.scss"
 import { useAuth } from "auth"
-import { truncate } from "@terra-money/terra-utils"
 import { addressFromWords } from "utils/bech32"
 import { useNavigate } from "react-router-dom"
 
