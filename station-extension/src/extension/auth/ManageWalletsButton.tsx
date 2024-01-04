96296ba192a5a151781c996d6cc29aed71b8e07e
plubber <er.h.gorski@gmail.com>
2024-01-04 15:02:49 -0500

    Refactor DashboardButton, PreferencesButton, ManageWalletsButton, and Tooltip components

--- station-extension/src/extension/auth/ManageWalletsButton.tsx
+++ station-extension/src/extension/auth/ManageWalletsButton.tsx
@@ -1,43 +1,48 @@
-import { useAuth } from "auth"
-import { bech32 } from "bech32"
-import is from "auth/scripts/is"
-import styles from "./ManageWalletsButton.module.scss"
+import { useAuth } from "auth";
+import { bech32 } from "bech32";
+import is from "auth/scripts/is";
+import styles from "./ManageWalletsButton.module.scss";
 // icons
-import { ReactComponent as WalletIcon } from "styles/images/icons/Wallet.svg"
-import UsbIcon from "@mui/icons-material/Usb"
-import BluetoothIcon from "@mui/icons-material/Bluetooth"
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"
-import { useNavigate } from "react-router-dom"
+import { ReactComponent as WalletIcon } from "styles/images/icons/Wallet.svg";
+import UsbIcon from "@mui/icons-material/Usb";
+import BluetoothIcon from "@mui/icons-material/Bluetooth";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { useNavigate } from "react-router-dom";
+import { forwardRef, ForwardedRef } from "react";
+import { useTranslation } from "react-i18next";
 
-export default function ManageWalletsButton() {
-  const navigate = useNavigate()
-
-  const { wallet, wallets } = useAuth()
+const ManageWalletsButton = forwardRef((_, ref: ForwardedRef<HTMLButtonElement>) => {
+  const navigate = useNavigate();
+  const { wallet, wallets } = useAuth();
+  const { t } = useTranslation();
 
   const selectedWallet = wallets.find((w) => {
     if ("words" in w) {
-      return w.words["330"] === wallet?.words["330"]
+      return w.words["330"] === wallet?.words["330"];
     } else {
       return (
         Buffer.from(bech32.decode(w.address).words).toString("hex") ===
         wallet?.words["330"]
-      )
+      );
     }
-  })
-  const isLedger = is.ledger(wallet)
+  });
 
-  if (!selectedWallet && !isLedger)
+  const isLedger = is.ledger(wallet);
+
+  if (!selectedWallet && !isLedger) {
     return (
-      <button className={styles.manage__wallets} onClick={() => navigate("/")}>
-        <WalletIcon style={{ fontSize: 18 }} /> Connect wallet
+      <button className={styles.manage__wallets} onClick={() => navigate("/")} ref={ref}>
+        <WalletIcon style={{ fontSize: 18 }} /> {t("Connect wallet")}
       </button>
-    )
+    );
+  }
 
   return (
     <button
       className={styles.manage__wallets}
       data-testid="manage-wallets-button"
       onClick={() => navigate("/manage-wallet/select")}
+      ref={ref}
     >
       {isLedger ? (
         wallet.bluetooth ? (
@@ -53,5 +58,7 @@ export default function ManageWalletsButton() {
         <ArrowDropDownIcon />
       </div>
     </button>
-  )
-}
+  );
+});
+
+export default ManageWalletsButton;
