41a6fc8497305225282efbadffe3d148dbdff3db
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2024-01-06 11:16:20 +0100

    cleanup

--- src/app/sections/EnableCoinType.tsx
+++ src/app/sections/EnableCoinType.tsx
@@ -3,17 +3,12 @@ import { ModalButton } from "@terra-money/station-ui"
 import { Button } from "components/general"
 import is from "auth/scripts/is"
 import CoinTypePasswordForm from "./CoinTypePasswordForm"
-import { useState } from "react"
 import { useAuth } from "auth"
 import CoinTypeMnemonicForm from "./CoinTypeMnemonicForm"
 
 const EnableCoinType = () => {
   const { t } = useTranslation()
   const { wallet } = useAuth()
-
-  const [modalKey, setModalKey] = useState(0)
-  const closeModal = () => setModalKey((k) => k + 1)
-
   // if wallet is ledger or multisig, return null
   if (!wallet || is.ledger(wallet) || is.multisig(wallet)) return null
   // if wallet already has injective address enabled, return null
