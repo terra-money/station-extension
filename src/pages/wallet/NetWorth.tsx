791a2cf25b09f4bf39bb4425cd24d9ff9b869091
plubber <er.h.gorski@gmail.com>
2024-01-10 17:14:56 -0500

    fix: portfolio icon

--- src/pages/wallet/NetWorth.tsx
+++ src/pages/wallet/NetWorth.tsx
@@ -5,9 +5,10 @@ import { useCurrency } from "data/settings/Currency"
 import { TooltipIcon } from "components/display"
 import { Read } from "components/token"
 import { usePortfolioValue } from "data/token"
-import { FlexColumn } from "@terra-money/station-ui"
-import NetWorthTooltip from "./NetWorthTooltip"
+import { Flex, FlexColumn, Tooltip } from "@terra-money/station-ui"
 import styles from "./NetWorth.module.scss"
+import { ForwardedRef, forwardRef } from "react"
+import { InfoIcon } from "@terra-money/station-ui"
 
 const NetWorth = () => {
   const { t } = useTranslation()
@@ -29,12 +30,22 @@ const NetWorth = () => {
     </h1>
   )
 
+  const PortfolioIcon = (forwardRef(
+    (_: any, ref: ForwardedRef<HTMLButtonElement>) => {
+      return (<button ref={ref}>
+        <InfoIcon  style={{fill: "var(--token-dark-900)"}}/>
+      </button>
+      )
+    }
+  ))
+
   return (
     <article className={styles.networth}>
       <FlexColumn justify="center" gap={8}>
-        <TooltipIcon content={<NetWorthTooltip />} placement="bottom">
-          <p>{capitalize(t("portfolio"))}</p>
-        </TooltipIcon>
+        <Flex gap={12}>
+        <p>{capitalize(t("portfolio"))}</p>
+        <Tooltip children={<PortfolioIcon />} content={t("Portfolio value is the total value of your assets minus staked and unstaking tokens")}/>
+        </Flex>
         <NetWorth />
       </FlexColumn>
       <div className={styles.networth__buttons}>
