a65023167e3e4d3e21782bd3b39ea8ef3b5a56a0
plubber <er.h.gorski@gmail.com>
2024-01-11 14:58:50 -0500

    Fix pre-selected asset value in SendPage/Address component and update send button onClick handler in WalletActionButtons component

--- station-extension/src/pages/wallet/SendPage/Address.tsx
+++ station-extension/src/pages/wallet/SendPage/Address.tsx
@@ -19,7 +19,7 @@ import { useLocation } from "react-router-dom"
 
 const Address = () => {
   const { form, goToStep, getWalletName, networks } = useSend()
-  const { state } = useLocation()
+  const { state: denom } = useLocation()
   const { recipients } = useRecentRecipients()
   const { register, setValue, formState, watch, trigger } = form
   const { errors } = formState
@@ -27,8 +27,8 @@ const Address = () => {
   const { t } = useTranslation()
 
   useEffect(() => {
-    setValue("asset", state?.denom)
-  }, [state?.denom, setValue])
+    setValue("asset", denom) // pre-selected from asset page
+  }, [denom, setValue])
 
   const [tab, setTab] = useState("wallets")
 
