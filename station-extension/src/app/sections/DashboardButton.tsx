b9390e9486c10866466e99491f9b919e3d88c9dc
plubber <er.h.gorski@gmail.com>
2024-01-16 15:34:01 -0500

    fix: icons

--- station-extension/src/app/sections/DashboardButton.tsx
+++ station-extension/src/app/sections/DashboardButton.tsx
@@ -1,15 +1,17 @@
-import { ForwardedRef, forwardRef } from 'react';
+import { ForwardedRef, forwardRef } from "react"
 import { DashboardIcon } from "@terra-money/station-ui"
-import HeaderIconButton from "app/components/HeaderIconButton";
-import { STATION } from "config/constants";
+import HeaderIconButton from "app/components/HeaderIconButton"
+import { STATION } from "config/constants"
 
-const DashboardButton = forwardRef((_, ref: ForwardedRef<HTMLButtonElement>) => {
-    const openDashboard = () =>  window.open(STATION, "_blank");
+const DashboardButton = forwardRef(
+  (_, ref: ForwardedRef<HTMLButtonElement>) => {
+    const openDashboard = () => window.open(STATION, "_blank")
     return (
-        <HeaderIconButton onClick={openDashboard} ref={ref}>
-            <DashboardIcon style={{ height: 18, width: 18, fill: "var(--token-dark-900)" }} />
-        </HeaderIconButton>
-    );
-});
+      <HeaderIconButton onClick={openDashboard} ref={ref}>
+        <DashboardIcon height={18} width={18} fill={"var(--token-dark-900)"} />
+      </HeaderIconButton>
+    )
+  }
+)
 
-export default DashboardButton;
+export default DashboardButton
