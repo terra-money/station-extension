e38cf6524193f57e22e48d398c599a97ff2199ea
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2024-02-08 13:34:21 -0300

    fix: executable logic

--- terra-utils/src/message.ts
+++ terra-utils/src/message.ts
@@ -330,8 +330,10 @@ export const getCanonicalMsg = (
             }
           } else {
             /* ------------------------- Contract Execution ------------------------- */
-            const executables = [...Object.keys((msg as any).msg || {}), ...Object.keys((msg as any).execute_msg || {})].length ?
-              [...Object.keys((msg as any).msg || {}), ...Object.keys((msg as any).execute_msg || {})] : 
+            const executables = Object.keys((msg as any).msg || {}).length ?
+              Object.keys((msg as any).msg) :
+              Object.keys((msg as any).execute_msg || {}).length ?
+              Object.keys((msg as any).execute_msg) :
               ["unknown_executable"]
             for (const executable of executables) {
               if (!["increase_allowance"].includes(executable))
