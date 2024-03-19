ce38063883ec47740be93822207ba36ce2cb93a0
plubber <er.h.gorski@gmail.com>
2024-03-19 16:27:09 -0400

    Add WalletNameInput component and update ManageWalletsPage

--- src/extension/auth/ManageWalletsPage.tsx
+++ src/extension/auth/ManageWalletsPage.tsx
@@ -3,6 +3,8 @@ import { useNavigate, useParams } from "react-router-dom"
 import { useManageWallet } from "auth/modules/manage/ManageWallets"
 import ExtensionList from "extension/components/ExtensionList"
 import ExtensionPageV2 from "extension/components/ExtensionPageV2"
+import { Grid } from "@terra-money/station-ui"
+import WalletNameInput from "./WalletNameInput"
 
 export default function ManageWalletsPage() {
   const { t } = useTranslation()
@@ -22,7 +24,10 @@ export default function ManageWalletsPage() {
       backButtonPath="/manage-wallet/select"
       subtitle={wallet}
     >
-      <ExtensionList list={manageWallet} />
+      <Grid gap={16}>
+        <WalletNameInput />
+        <ExtensionList list={manageWallet} />
+      </Grid>
     </ExtensionPageV2>
   )
 }
