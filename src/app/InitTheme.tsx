e8c9b7ba779b08b6e22f50a5301975dc30dca29b
plubber <er.h.gorski@gmail.com>
2024-03-28 15:23:25 -0400

    remove unused

--- src/app/InitTheme.tsx
+++ src/app/InitTheme.tsx
@@ -1,15 +1,12 @@
 import { useEffect } from "react"
-import { DefaultTheme } from "utils/localStorage"
-import { useThemeState, useValidateTheme } from "data/settings/Theme"
+import { useThemeState } from "data/settings/Theme"
 
 const InitTheme = () => {
   const [theme, setTheme] = useThemeState()
-  const validate = useValidateTheme()
-  const valid = validate(theme)
 
   useEffect(() => {
-    setTheme(valid ? theme : DefaultTheme)
-  }, [valid]) // eslint-disable-line
+    setTheme(theme)
+  }, [theme]) // eslint-disable-line
 
   return null
 }
